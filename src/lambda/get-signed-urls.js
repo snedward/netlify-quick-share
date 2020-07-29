fce6f0e226260fd973d3895c1cf327f1ba555130
Edward Blair <67782560+snedward@users.noreply.github.com>
2020-07-29 10:03:17 +0100

    Update get-signed-urls.js

--- src/lambda/get-signed-urls.js
+++ src/lambda/get-signed-urls.js
@@ -17,7 +17,7 @@ var cuid = require( "cuid" );
 var s3 = new AWS.S3({
 	accessKeyId: process.env.AWS_S3_ACCESS_KEY,
 	secretAccessKey: process.env.AWS_S3_SECRET_KEY,
-	region: "us-east-1"
+	region: "eu-west-2"
 });
 
 // I am the Netlify Function handler.
